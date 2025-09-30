baf9f6b9e05411b660c07eb8f06b1b42aa0726b7
amrigosss <amrigosss@gmail.com>
2025-09-30 19:28:01 +0800

    Added starter pages and styles

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Simple JS placeholder
+console.log("Coffee Shop website loaded.");
+
+// Example: Menu filter (to be expanded later)
+function filterMenu(category) {
+  alert("Filter menu by: " + category);
+}
